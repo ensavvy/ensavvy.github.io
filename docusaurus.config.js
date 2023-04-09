32dd6f21ca6d143ebc5479c05f32b86199faaac9
adtaco <svc_admin@adtaco.com>
2023-04-09 13:15:22 +0800

    Initial GitHub actions autodeploy to Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,6 +103,9 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        disableSwitch: false,
+      },
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
